51b7f7e09771bbccc9bb6ef233e190dd468b29a7
Aleksandr Krapivin <96174983+smoozmy@users.noreply.github.com>
2024-08-26 19:50:14 +0300

    Добавлена разделительная линия в TBC

--- Tracker/TabBarController.swift
+++ Tracker/TabBarController.swift
@@ -9,16 +9,15 @@ class TabBarController: UITabBarController {
         let trackers = TrackersViewController()
         trackers.title = "Трекеры"
         trackers.tabBarItem.image = UIImage(named: "trackers")
+        let trackersNav = UINavigationController(rootViewController: trackers)
         
         
         let statistics = StatisticsViewController()
         statistics.title = "Статистика"
         statistics.tabBarItem.image = UIImage(named: "stats")
+        let statisticsNav = UINavigationController(rootViewController: statistics)
         
-//        _ = UINavigationController(rootViewController: trackers)
-//        _ = UINavigationController(rootViewController: statistics)
-//        
-        viewControllers = [trackers, statistics]
+        viewControllers = [trackersNav, statisticsNav]
         
         configureTabBarAppearance()
     }
@@ -26,5 +25,15 @@ class TabBarController: UITabBarController {
     private func configureTabBarAppearance() {
         let tabBar = self.tabBar
         tabBar.tintColor = .blueYP
+        tabBar.unselectedItemTintColor = .grayYP
+        
+        if #available(iOS 13.0, *) {
+            let appearance = UITabBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            tabBar.standardAppearance = appearance
+            if #available(iOS 15.0, *) {
+                tabBar.scrollEdgeAppearance = tabBar.standardAppearance
+            }
+        }
     }
 }
