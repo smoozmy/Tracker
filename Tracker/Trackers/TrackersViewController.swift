357ae97f4c2d9aa52c0a0470413e161015e9d746
Aleksandr Krapivin <96174983+smoozmy@users.noreply.github.com>
2024-08-28 14:19:06 +0300

    Сверстан экран Трекеры

--- Tracker/Trackers/TrackersViewController.swift
+++ Tracker/Trackers/TrackersViewController.swift
@@ -2,6 +2,71 @@ import UIKit
 
 final class TrackersViewController: UIViewController {
     
+    private lazy var mainStackView: UIStackView = {
+        let element = UIStackView()
+        element.axis = .vertical
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var stubStackView: UIStackView = {
+        let element = UIStackView()
+        element.axis = .vertical
+        element.spacing = 8
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var stubImage: UIImageView = {
+        let element = UIImageView()
+        element.image = UIImage(named: "EmptyStub")
+        element.contentMode = .scaleAspectFit
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var stubLabel: UILabel = {
+        let element = UILabel()
+        element.text = "Что будем отслеживать?"
+        element.textAlignment = .center
+        element.textColor = .blackYPDay
+        element.font = .systemFont(ofSize: 12, weight: .medium)
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var scrollView: UIScrollView = {
+        let element = UIScrollView()
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let element = UILabel()
+        element.text = "Трекеры"
+        element.textColor = .blackYPDay
+        element.font = .systemFont(ofSize: 34, weight: .bold)
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var searchBar: UISearchBar = {
+        let element = UISearchBar()
+        element.placeholder = "Поиск"
+        element.searchBarStyle = .minimal
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    
+    
     // MARK: - UI and Lyfe Cycle
     
     override func viewDidLoad() {
@@ -9,9 +74,43 @@ final class TrackersViewController: UIViewController {
         
         setView()
         setupConstraints()
+        
+        setupNavigationBar()
+        showStub()
     }
+    
+    private func setupNavigationBar() {
+        navigationItem.title = nil
+        
+        let addButton = UIBarButtonItem(
+            image: UIImage(named: "plus"),
+            style: .plain,
+            target: self,
+            action: #selector(addTrackerTapped)
+        )
+        addButton.tintColor = .blackYPDay
+        
+        navigationItem.leftBarButtonItem = addButton
+    }
+    
+
+    
+    @objc private func addTrackerTapped() {
+        print("Добавить трекер")
+    }
+    
+    private func showStub() {
+        stubStackView.isHidden = false
+    }
+    
     private func setView() {
-        // Здесь добавляются новые элементы для отображения на экране
+        view.addSubview(mainStackView)
+        mainStackView.addArrangedSubview(titleLabel)
+        mainStackView.addArrangedSubview(searchBar)
+        mainStackView.addArrangedSubview(scrollView)
+        scrollView.addSubview(stubStackView)
+        stubStackView.addArrangedSubview(stubImage)
+        stubStackView.addArrangedSubview(stubLabel)
     }
     
     // MARK: - Actions
@@ -23,7 +122,24 @@ final class TrackersViewController: UIViewController {
 extension TrackersViewController {
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            // Здесь прописываются констрейнты для элементов
+            mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            mainStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            titleLabel.topAnchor.constraint(equalTo: mainStackView.topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: mainStackView.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: mainStackView.trailingAnchor),
+            
+            searchBar.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 7),
+            
+            stubStackView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            stubStackView.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor),
+            stubStackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            stubStackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            stubImage.widthAnchor.constraint(equalToConstant: 80),
+            stubImage.heightAnchor.constraint(equalToConstant: 80),
+                    
         ])
     }
 }
